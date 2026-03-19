4bbc4e2714b23a65a64921f494709e192e4f8b13
Rafael Blasques Morales <rafaelblasques0611@gmail.com>
2026-03-19 20:02:38 -0300

    clienteRepository

---
+++ java/src/main/java/com/atividade/java/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.atividade.java.repositories;
+
+import com.atividade.java.models.ClienteModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
+}
